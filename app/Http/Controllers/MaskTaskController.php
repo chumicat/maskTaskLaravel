7edc574a319cee1d02bf0476734c69ba5f651fcd
RUSSELL LI-SHUO TSENG <russell57260620@gmail.com>
2020-03-30 06:42:54 +0800

    autosave

--- app/Http/Controllers/MaskTaskController.php
+++ app/Http/Controllers/MaskTaskController.php
@@ -8,6 +8,32 @@ const FILE_NAME = "maskdata.csv";
 class MaskTaskController extends Controller {
     private $data;
     private $lastUpdateTime;
+    private function adjustText($text = null)
+    {
+        // 我把大部分時間用在共同專案上了
+        // 沒時間了，所以不整理
+        // 寫測試到快死了～～
+        if ($text === null) {
+            foreach ($this->data as &$row) {
+                $row["機構名稱"] = str_replace(["Ｏ", "0", "˙", "．", "，", "-", "－"], ["零", "零", "、", "、", "、", "之", "之"], $row["機構名稱"]);
+                $row["機構名稱"] = str_replace(["０", "１", "２", "３", "４", "５", "６", "７", "８", "９"], ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"], $row["機構名稱"]);
+                $row["機構名稱"] = str_replace(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"], $row["機構名稱"]);
+                $row["機構名稱"] = str_replace(["台", "F", "f", "Ｆ", "ｆ"], ["臺", "樓", "樓", "樓", "樓"], $row["機構名稱"]);
+                $row["機構地址"] = str_replace(["Ｏ", "0", "˙", "．", "，", "-", "－"], ["零", "零", "、", "、", "、", "之", "之"], $row["機構地址"]);
+                $row["機構地址"] = str_replace(["０", "１", "２", "３", "４", "５", "６", "７", "８", "９"], ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"], $row["機構地址"]);
+                $row["機構地址"] = str_replace(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"], $row["機構地址"]);
+                $row["機構地址"] = str_replace(["台", "F", "f", "Ｆ", "ｆ"], ["臺", "樓", "樓", "樓", "樓"], $row["機構地址"]);
+            }
+            return;
+        }
+        $text = str_replace(["Ｏ", "0", "˙", "．", "，", "-", "－"], ["零", "零", "、", "、
+        ", "、", "之", "之"], $text);
+        $text = str_replace(["０", "１", "２", "３", "４", "５", "６", "７", "８", "９"], ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"], $text);
+        $text = str_replace(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"], $text);
+        $text = str_replace(["台", "F", "f", "Ｆ", "ｆ"], ["臺", "樓", "樓", "樓", "樓"], $text);
+        return $text;
+    }
+
     public function filtering($header, $keyword) {
         $ret = [];
         foreach ($this->data as $row) {
@@ -19,10 +45,10 @@ class MaskTaskController extends Controller {
 
     public function filter(Request $r) {
         if ($r->query('i')) {
-            $this->filtering("機構名稱", $r->query('i'));
+            $this->filtering("機構名稱", $this->adjustText($r->query('i')));
         }
         if ($r->query('d')) {
-            $this->filtering("機構地址", $r->query('d'));
+            $this->filtering("機構地址", $this->adjustText($r->query('d')));
         }
     }
 
@@ -62,7 +88,11 @@ class MaskTaskController extends Controller {
             $this->data[$rowid]["孩童口罩"] = (int)$row[5];
             $this->lastUpdateTime = $row[6];
         }
+        $this->adjustText();
         $this->filter($r);
+        usort($this->data, function ($a, $b) {
+            return $a["成人口罩"] < $b["成人口罩"];
+        });
         $this->show($r);
     }
 }

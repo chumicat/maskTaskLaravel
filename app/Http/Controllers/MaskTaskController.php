8eb5b3b34b5d1472742e94105277a8472d08b076
chumicat <russell.tseng@104.com.tw>
2020-03-30 14:50:14 +0800

    update from 104

--- app/Http/Controllers/MaskTaskController.php
+++ app/Http/Controllers/MaskTaskController.php
@@ -5,33 +5,120 @@ use Exception;
 const MAX_ROW_LEN = 1000;
 const FILE_NAME = "maskdata.csv";
 
+
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Mask Task Laravel",
+ *      description="A Laravel Version Mask Map",
+ *      @OA\Contact(
+ *          email="russell.tseng@104.com.tw"
+ *      ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ */
+
+/**
+ *  @OA\Server(
+ *      url="http://localhost",
+ *      description="Local Host"
+ *  )
+ *
+ *  @OA\Server(
+*      url="https://projects.dev/api/v1",
+ *      description="L5 Swagger OpenApi Server"
+ * )
+ */
+
+/**
+ * @OA\SecurityScheme(
+ *     type="oauth2",
+ *     description="Use a global client_id / client_secret and your username / password combo to obtain a token",
+ *     name="Password Based",
+ *     in="header",
+ *     scheme="https",
+ *     securityScheme="Password Based",
+ *     @OA\Flow(
+ *         flow="password",
+ *         authorizationUrl="/oauth/authorize",
+ *         tokenUrl="/oauth/token",
+ *         refreshUrl="/oauth/token/refresh",
+ *         scopes={}
+ *     )
+ * )
+ */
+
+/**
+ * @OA\Tag(
+ *     name="Mask Task Project",
+ *     description="A Laravel Version Mask Map"
+ * )
+ * 
+ * @OA\ExternalDocumentation(
+ *     description="To the project->",
+ *     url="https://github.com/chumicat/maskTaskLaravel"
+ * )
+ */
+
+/**
+ * @OA\Get(
+ *      path="/maskTask",
+ *      operationId="getProjectsList",
+ *      tags={"Projects"},
+ *      summary="Search Mask",
+ *      description="Returns corresponded data",
+ *      @OA\Parameter(
+ *          name="i",
+ *          description="institution keyword",
+ *          required=false,
+ *          in="query",
+ *          @OA\Schema(
+ *              type="string"
+ *          )
+ *      ),
+ *      @OA\Parameter(
+ *          name="a",
+ *          description="address keyword",
+ *          required=false,
+ *          in="query",
+ *          @OA\Schema(
+ *              type="string"
+ *          )
+ *      ),
+ *      @OA\Response(response=200, description="successful operation"),
+ *      @OA\Response(response=400, description="Bad request"),
+ *      @OA\Response(response=404, description="Resource Not Found"),
+ *      security={
+ *           {"api_key_security_example": {}}
+ *      }
+ * )
+ *
+ * Returns list of projects
+ */
+
+
 class MaskTaskController extends Controller {
     private $data;
     private $lastUpdateTime;
     private function adjustText($text = null)
     {
-        // 我把大部分時間用在共同專案上了
-        // 沒時間了，所以不整理
-        // 寫測試到快死了～～
-        if ($text === null) {
-            foreach ($this->data as &$row) {
-                $row["機構名稱"] = str_replace(["Ｏ", "0", "˙", "．", "，", "-", "－"], ["零", "零", "、", "、", "、", "之", "之"], $row["機構名稱"]);
-                $row["機構名稱"] = str_replace(["０", "１", "２", "３", "４", "５", "６", "７", "８", "９"], ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"], $row["機構名稱"]);
-                $row["機構名稱"] = str_replace(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"], $row["機構名稱"]);
-                $row["機構名稱"] = str_replace(["台", "F", "f", "Ｆ", "ｆ"], ["臺", "樓", "樓", "樓", "樓"], $row["機構名稱"]);
-                $row["機構地址"] = str_replace(["Ｏ", "0", "˙", "．", "，", "-", "－"], ["零", "零", "、", "、", "、", "之", "之"], $row["機構地址"]);
-                $row["機構地址"] = str_replace(["０", "１", "２", "３", "４", "５", "６", "７", "８", "９"], ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"], $row["機構地址"]);
-                $row["機構地址"] = str_replace(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"], $row["機構地址"]);
-                $row["機構地址"] = str_replace(["台", "F", "f", "Ｆ", "ｆ"], ["臺", "樓", "樓", "樓", "樓"], $row["機構地址"]);
-            }
-            return;
-        }
         $text = str_replace(["Ｏ", "0", "˙", "．", "，", "-", "－"], ["零", "零", "、", "、
         ", "、", "之", "之"], $text);
         $text = str_replace(["０", "１", "２", "３", "４", "５", "６", "７", "８", "９"], ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"], $text);
         $text = str_replace(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"], $text);
         $text = str_replace(["台", "F", "f", "Ｆ", "ｆ"], ["臺", "樓", "樓", "樓", "樓"], $text);
         return $text;
+    }
+
+    private function adjustdata()
+    {
+        foreach ($this->data as &$row) {
+            $row["機構名稱"] = $this->adjustText($row["機構名稱"]);
+            $row["機構地址"] = $this->adjustText($row["機構地址"]);
+        }
     }
 
     public function filtering($header, $keyword) {
@@ -88,7 +175,7 @@ class MaskTaskController extends Controller {
             $this->data[$rowid]["孩童口罩"] = (int)$row[5];
             $this->lastUpdateTime = $row[6];
         }
-        $this->adjustText();
+        $this->adjustdata();
         $this->filter($r);
         usort($this->data, function ($a, $b) {
             return $a["成人口罩"] < $b["成人口罩"];
